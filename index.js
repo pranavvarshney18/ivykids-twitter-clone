05cbe8395c343aeadf219ca3867c586899608a24
pranavvarshney18 <pranavvarshney18@gmail.com>
2023-08-20 12:16:31 +0530

    setup ejs and view engine

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3000;
+const expressLayouts = require('express-ejs-layouts');
 
+
+//rendering views
+app.use(expressLayouts);
+//place css and js tags at their respective postions
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
+
+//use express router
+app.use('/', require('./routes/home.js'));
+//all static files are present inside assets folder
+app.use(express.static('./assets'));
+
+
+//setup view engine
+app.set('view engine', 'ejs');
+app.set('views', './views');
 
 
 app.listen(port, (err) =>{
